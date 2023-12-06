b292285a9c04fc67d88669e60968b07c1fc80afd
Carlos <carlosgemano2003@gmail.com>
2023-12-06 10:51:53 -0300

    Projeto adocao

--- FormPage/FormPage.js
+++ FormPage/FormPage.js
@@ -1,3 +1,18 @@
+function resetStyles() {
+    let nomeError = document.getElementById("nameError")
+    let senhaError = document.getElementById("senhaError")
+    let senhaRepetirError = document.getElementById("senhaRepetirError")
+    let emailError = document.getElementById("emailError")
+    let dataError = document.getElementById("dataError")
+
+    nomeError.style.opacity = "0"
+    senhaError.style.opacity = "0"
+    senhaRepetirError.style.opacity = "0"
+    emailError.style.opacity = "0"
+    dataError.style.opacity = "0"
+
+}
+
 function submitForm() {
 
     let nomeError = document.getElementById("nameError")
@@ -12,10 +27,12 @@ function submitForm() {
     let emailInput = document.getElementById("emailInput")
     let dataInput = document.getElementById("dataInput")
 
+    resetStyles()
+
     if (nomeInput.value.trim() == "") {
         nomeError.style.opacity = "1"
     }
-    if (senhaInput.value.trim() == "") {
+    if (senhaInput.value.trim() == "" || senhaInput.value.trim() <= 8) {
         senhaError.style.opacity = "1"
     }
     if (senhaRepetirInput.value.trim() == "" || senhaRepetirInput.value !== senhaInput.value) {
@@ -27,5 +44,7 @@ function submitForm() {
     if (dataInput.value.trim() == "") {
         dataError.style.opacity = "1"
     }
+
+    
 
 }
